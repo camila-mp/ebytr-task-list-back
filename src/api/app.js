5f940717141a526e8f8ade2df139b29dc392835f
camila-mp <ca.malvp@gmail.com>
2022-02-16 10:10:49 -0300

    adiciona usersRouter no app

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const errorHandler = require('./middlewares/errorHandler/errorHandler');
+const usersRouter = require('./controllers/users/router');
 
 const app = express();
 
 app.use(express.json());
 
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 // app.use('/login', loginRouter);
 // app.use('/lists', listsRouter);
 
