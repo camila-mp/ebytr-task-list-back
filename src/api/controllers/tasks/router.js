225a9ee6c42e876f42021b485e50d7d2bba07ef3
camila-mp <ca.malvp@gmail.com>
2022-02-20 12:48:34 -0300

    modifica detalhe no router de 'tasks'

--- src/api/controllers/tasks/router.js
+++ src/api/controllers/tasks/router.js
@@ -3,14 +3,14 @@ const tokenValidation = require('../../middlewares/validations/tokenValidation')
 const idValidation = require('../../middlewares/validations/idValidation');
 const createNewTask = require('./createTask');
 const getAllTasks = require('./getAllTasks');
-const updateTaskStatus = require('./updateTask');
+const updateTask = require('./updateTask');
 const deleteTask = require('./deleteTask');
 
 const router = express.Router();
 
 router.post('/', tokenValidation, createNewTask);
 router.get('/', tokenValidation, getAllTasks);
-router.patch('/update/:id', idValidation, tokenValidation, updateTaskStatus);
+router.patch('/update/:id', idValidation, tokenValidation, updateTask);
 router.delete('/:id', idValidation, tokenValidation, deleteTask);
 
 module.exports = router;
