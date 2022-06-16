4ce755787a5f0a81f7402da06db76853573cd59b
camila-mp <ca.malvp@gmail.com>
2022-06-16 10:46:18 -0300

    altera console.log do server

--- src/api/server.js
+++ src/api/server.js
@@ -3,4 +3,4 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
+app.listen(PORT, () => console.log(`serving at port ${PORT}`));
