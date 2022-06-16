227f236a872bc268336615df2b679de66a1d2a1b
camila-mp <ca.malvp@gmail.com>
2022-06-16 14:40:31 -0300

    adiciona logica para verificar userId no service updateTask

--- src/api/services/tasks/updateTask.js
+++ src/api/services/tasks/updateTask.js
@@ -1,13 +1,21 @@
 const updateTaskModel = require('../../models/tasks/updateTask');
+const getById = require('../../models/tasks/getById');
 const { StatusCodes } = require('http-status-codes');
 
-module.exports = async (id, taskData, userId) => {
-  const updateTask = await updateTaskModel(id, taskData);
+module.exports = async (id, taskData, crrUserId) => {
+  const targetTask = await getById(id);
+  const { userId } = targetTask;
 
-  if (updateTask.modifiedCount === 0) {
-    return { code: StatusCodes.NOT_FOUND, message: 'Sorry, no task with the provided id was found.' }
-  } else if (updateTask.modifiedCount > 0) {
-    return { code: StatusCodes.OK, message: 'Task was successfully modified.'}
+  if (crrUserId.toString() === userId.toString()) {
+    const updateResult = await updateTaskModel(id, taskData);
+    if (updateResult.modifiedCount === 0) {
+      return { code: StatusCodes.NOT_FOUND, message: 'Sorry, no task with the provided id was found.' }
+    } else if (updateResult.modifiedCount > 0) {
+      return { code: StatusCodes.OK, message: 'Task was successfully modified.'}
+    }
   }
-  return updateTask;
+
+  if (crrUserId.toString() !== userId.toString()) {
+    return false;
+  }
 };
