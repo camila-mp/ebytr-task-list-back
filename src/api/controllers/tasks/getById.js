b5d1bf6151fc3139ce4b9c2c8f7acbfcbe634c7b
camila-mp <ca.malvp@gmail.com>
2022-06-16 14:42:13 -0300

    adiciona logica para verificar userId

--- src/api/controllers/tasks/getById.js
+++ src/api/controllers/tasks/getById.js
@@ -3,9 +3,18 @@ const getTask = require('../../services/tasks/getById');
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
+  const { userId } = req;
 
   try {
-    const task = await getTask(id);
+    const task = await getTask(id, userId);
+
+    if (!task) {
+      const err = { 
+        code: StatusCodes.UNAUTHORIZED,
+        message: 'Sorry, you are not allowed to read this task because you are not the author.'
+      }
+      return next(err)
+    }
 
     return res.status(StatusCodes.OK).json(task);
   } catch (err) {
--- src/api/services/tasks/getById.js
+++ src/api/services/tasks/getById.js
@@ -1,6 +1,14 @@
 const getTaskById = require('../../models/tasks/getById');
 
-module.exports = async (id) => {
+module.exports = async (id, crrUserId) => {
   const task = await getTaskById(id);
-  return task;
+  const { userId } = task;
+
+  if (crrUserId.toString() === userId.toString()) {
+    return getTaskById(id)
+  }
+
+  else if (crrUserId.toString() !== userId.toString()) {
+    return false; 
+  }
 };
