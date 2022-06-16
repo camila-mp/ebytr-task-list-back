de3c22343985325ff9a1d6b063ebe941f19d1e7c
camila-mp <ca.malvp@gmail.com>
2022-06-16 11:45:07 -0300

    adiciona novo endpoint GET (/tasks/:id) ao controllers/tasks/router

--- src/api/controllers/tasks/router.js
+++ src/api/controllers/tasks/router.js
@@ -3,6 +3,7 @@ const tokenValidation = require('../../middlewares/validations/tokenValidation')
 const idValidation = require('../../middlewares/validations/idValidation');
 const createNewTask = require('./createTask');
 const getAllTasks = require('./getAllTasks');
+const getTaskById = require('./getById');
 const updateTask = require('./updateTask');
 const deleteTask = require('./deleteTask');
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.post('/', tokenValidation, createNewTask);
 router.get('/', tokenValidation, getAllTasks);
+router.get('/:id', idValidation, tokenValidation, getTaskById);
 router.patch('/update/:id', idValidation, tokenValidation, updateTask);
 router.delete('/:id', idValidation, tokenValidation, deleteTask);
 
